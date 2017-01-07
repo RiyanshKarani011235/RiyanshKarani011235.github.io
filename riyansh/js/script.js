8ee31c0067085895f411b898eb431a115e679b3c
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2017-01-07 17:29:49 +0530

    done everything for now. Todo next : 1. Change content for final year project (grammar models and stuff 2. Change content for IITB internship 3. Add provision for adding different colored underlinds

--- riyansh/js/script.js
+++ riyansh/js/script.js
@@ -13,22 +13,6 @@ var scrollDown = function() {
 $(window).load(function () {
 	new Notebook(3, 6, 3);
 
-	// set eventListeners for navigation buttons
-	// up navigation button
-	$('#up_navigation_button').click(function() {
-		scrollUp();
-	});
-
-	// down navigation button
-	$('#down_navigation_button').click(function() {
-		scrollDown();
-	});
-
-	$('#github_icon').click(function() {
-		console.log('github icon clicked');
-		scrollDown();
-	});
-
 	// if an anchor tag with href containing id of another element on the 
 	// page is clicked, then scroll to that part instead of the default
 	// behaviour
@@ -36,7 +20,7 @@ $(window).load(function () {
 		if($.attr(this, 'href').charAt(0) == '#') {
 	    	event.preventDefault();
 	    	$('html, body').animate({
-		        scrollTop: $( $.attr(this, 'href') ).offset().top
+		        scrollTop: $( $.attr(this, 'href') ).offset().top - 10
 		    }, 500);
 		}
 	});
